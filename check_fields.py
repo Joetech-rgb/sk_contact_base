f3df2e3ebe89fe3c191eae305e1903b8ad7516d2
Ankamah <ankamahjoseph731@gmail.com>
2026-06-19 09:11:05 -0800

    chore: remove debug and patch scripts from repo

--- check_fields.py
+++
@@ -1,9 +0,0 @@
-﻿import django, os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sk_contact_base.settings')
-django.setup()
-
-from contacts.models import Contact
-fields = [f.name for f in Contact._meta.get_fields()]
-print('Contact fields:')
-for f in fields:
-    print(' -', f)
